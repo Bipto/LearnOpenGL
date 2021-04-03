e713acafb3849493695e26d6e9dfaacca13097b8
Adam Beardow <abeardow13@gmail.com>
2021-04-03 19:14:24 +0100

    Creating a window

--- LearnOpenGL/Program.cs
+++ LearnOpenGL/Program.cs
@@ -1,12 +1,52 @@
-﻿using System;
+﻿using Silk.NET.Input;
+using Silk.NET.Windowing;
 
 namespace LearnOpenGL
 {
     class Program
     {
+        private static IWindow window;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var options = WindowOptions.Default;
+            options.Size = new Silk.NET.Maths.Vector2D<int>(800, 600);
+            options.Title = "LearnOpenGL with Silk.NET";
+
+            window = Window.Create(options);
+
+            window.Load += OnLoad;
+            window.Update += OnUpdate;
+            window.Render += OnRender;
+
+            window.Run();
+        }
+
+        private static void OnRender(double obj)
+        {
+            
+        }
+
+        private static void OnUpdate(double obj)
+        {
+            
+        }
+
+        private static void OnLoad()
+        {
+            IInputContext input = window.CreateInput();
+            for (int i = 0; i < input.Keyboards.Count; i++)
+            {
+                input.Keyboards[i].KeyDown += KeyDown;
+            }
+        }
+
+        private static void KeyDown(IKeyboard arg1, Key arg2, int arg3)
+        {
+            if (arg2 == Key.Escape)
+            {
+                window.Close();
+            }
         }
     }
 }
