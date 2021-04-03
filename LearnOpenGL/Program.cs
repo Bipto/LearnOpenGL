5499c21edba1ee395d2c3f1817e2c92cb62c9aa9
Adam Beardow <abeardow13@gmail.com>
2021-04-03 19:32:03 +0100

    Drawing a rectangle

--- LearnOpenGL/Program.cs
+++ LearnOpenGL/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Silk.NET.Input;
+using Silk.NET.OpenGL;
 using Silk.NET.Windowing;
 
 namespace LearnOpenGL
@@ -6,6 +7,44 @@ namespace LearnOpenGL
     class Program
     {
         private static IWindow window;
+        private static GL Gl;
+
+        private static uint Vbo;
+        private static uint Ebo;
+        private static uint Vao;
+        private static uint Shader;
+
+        private static readonly string VertexShaderSource = @"
+        #version 330 core
+        layout (location = 0) in vec4 vPos;
+
+        void main()
+        {
+            gl_Position = vec4(vPos.x, vPos.y, vPos.z, 1.0);
+        }";
+
+        private static readonly string FragmentShaderSource = @"
+        #version 330 core
+        out vec4 FragColor;
+
+        void main()
+        {
+            FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+        }";
+
+        private static readonly float[] Vertices =
+        {
+            0.5f, 0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f,
+            -0.5f, -0.5f, 0.0f,
+            -0.5f, 0.5f, 0.5f
+        };
+
+        private static readonly uint[] Indices =
+        {
+            0, 1, 3,
+            1, 2, 3
+        };
 
         static void Main(string[] args)
         {
@@ -18,13 +57,27 @@ namespace LearnOpenGL
             window.Load += OnLoad;
             window.Update += OnUpdate;
             window.Render += OnRender;
+            window.Closing += OnClose;
 
             window.Run();
         }
 
-        private static void OnRender(double obj)
+        private static void OnClose()
         {
-            
+            Gl.DeleteBuffer(Vbo);
+            Gl.DeleteBuffer(Ebo);
+            Gl.DeleteVertexArray(Vao);
+            Gl.DeleteProgram(Shader);
+        }
+
+        private static unsafe void OnRender(double obj)
+        {
+            Gl.Clear((uint)ClearBufferMask.ColorBufferBit);
+
+            Gl.BindVertexArray(Vao);
+            Gl.UseProgram(Shader);
+
+            Gl.DrawElements(PrimitiveType.Triangles, (uint)Indices.Length, DrawElementsType.UnsignedInt, null);
         }
 
         private static void OnUpdate(double obj)
@@ -32,13 +85,71 @@ namespace LearnOpenGL
             
         }
 
-        private static void OnLoad()
+        private static unsafe void OnLoad()
         {
             IInputContext input = window.CreateInput();
             for (int i = 0; i < input.Keyboards.Count; i++)
             {
                 input.Keyboards[i].KeyDown += KeyDown;
             }
+
+            Gl = GL.GetApi(window);
+
+            Vao = Gl.GenVertexArray();
+            Gl.BindVertexArray(Vao);
+
+            Vbo = Gl.GenBuffer();
+            Gl.BindBuffer(BufferTargetARB.ArrayBuffer, Vbo);
+            fixed (void* v = &Vertices[0])
+            {
+                Gl.BufferData(BufferTargetARB.ArrayBuffer, (nuint)(Vertices.Length * sizeof(uint)), v, BufferUsageARB.StaticDraw);
+            }
+
+            Ebo = Gl.GenBuffer();
+            Gl.BindBuffer(BufferTargetARB.ElementArrayBuffer, Ebo);
+            fixed (void* i = &Indices[0])
+            {
+                Gl.BufferData(BufferTargetARB.ElementArrayBuffer, (nuint)(Indices.Length * sizeof(uint)), i, BufferUsageARB.StaticDraw);
+            }
+
+            uint vertexShader = Gl.CreateShader(ShaderType.VertexShader);
+            Gl.ShaderSource(vertexShader, VertexShaderSource);
+            Gl.CompileShader(vertexShader);
+
+            string infoLog = Gl.GetShaderInfoLog(vertexShader);
+            if (!string.IsNullOrEmpty(infoLog))
+            {
+                System.Console.WriteLine($"Error compiling vertex shader {infoLog}");
+            }
+
+            uint fragmentShader = Gl.CreateShader(ShaderType.FragmentShader);
+            Gl.ShaderSource(fragmentShader, FragmentShaderSource);
+            Gl.CompileShader(fragmentShader);
+
+            infoLog = Gl.GetShaderInfoLog(fragmentShader);
+            if (!string.IsNullOrWhiteSpace(infoLog))
+            {
+                System.Console.WriteLine($"Error compiling fragment shader {infoLog}");
+            }
+
+            Shader = Gl.CreateProgram();
+            Gl.AttachShader(Shader, vertexShader);
+            Gl.AttachShader(Shader, fragmentShader);
+            Gl.LinkProgram(Shader);
+
+            Gl.GetProgram(Shader, GLEnum.LinkStatus, out var status);
+            if (status == 0)
+            {
+                System.Console.WriteLine($"Error linking shader {Gl.GetProgramInfoLog(Shader)}");
+            }
+
+            Gl.DetachShader(Shader, vertexShader);
+            Gl.DetachShader(Shader, fragmentShader);
+            Gl.DeleteShader(vertexShader);
+            Gl.DeleteShader(fragmentShader);
+
+            Gl.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), null);
+            Gl.EnableVertexAttribArray(0);
         }
 
         private static void KeyDown(IKeyboard arg1, Key arg2, int arg3)
