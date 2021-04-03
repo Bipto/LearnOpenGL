4f20732b65dc63901a3df3dd3349e7ad2599dbb9
Adam Beardow <abeardow13@gmail.com>
2021-04-03 20:56:54 +0100

    Using abstractions

--- LearnOpenGL/Program.cs
+++ LearnOpenGL/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Silk.NET.Input;
 using Silk.NET.OpenGL;
 using Silk.NET.Windowing;
+using System;
 
 namespace LearnOpenGL
 {
@@ -9,35 +10,17 @@ namespace LearnOpenGL
         private static IWindow window;
         private static GL Gl;
 
-        private static uint Vbo;
-        private static uint Ebo;
-        private static uint Vao;
-        private static uint Shader;
-
-        private static readonly string VertexShaderSource = @"
-        #version 330 core
-        layout (location = 0) in vec4 vPos;
-
-        void main()
-        {
-            gl_Position = vec4(vPos.x, vPos.y, vPos.z, 1.0);
-        }";
-
-        private static readonly string FragmentShaderSource = @"
-        #version 330 core
-        out vec4 FragColor;
-
-        void main()
-        {
-            FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
-        }";
+        private static BufferObject<float> Vbo;
+        private static BufferObject<uint> Ebo;
+        private static VertexArrayObject<float, uint> Vao;
+        private static Shader Shader;
 
         private static readonly float[] Vertices =
         {
-            0.5f, 0.5f, 0.0f,
-            0.5f, -0.5f, 0.0f,
-            -0.5f, -0.5f, 0.0f,
-            -0.5f, 0.5f, 0.5f
+            0.5f, 0.5f, 0.0f, 1, 0, 0, 1,
+            0.5f, -0.5f, 0.0f, 0, 0, 0, 1
+            -0.5f, -0.5f, 0.0f, 0, 0, 1, 1,
+            -0.5f, 0.5f, 0.5f, 0, 0, 0, 1,
         };
 
         private static readonly uint[] Indices =
@@ -55,7 +38,6 @@ namespace LearnOpenGL
             window = Window.Create(options);
 
             window.Load += OnLoad;
-            window.Update += OnUpdate;
             window.Render += OnRender;
             window.Closing += OnClose;
 
@@ -64,25 +46,21 @@ namespace LearnOpenGL
 
         private static void OnClose()
         {
-            Gl.DeleteBuffer(Vbo);
-            Gl.DeleteBuffer(Ebo);
-            Gl.DeleteVertexArray(Vao);
-            Gl.DeleteProgram(Shader);
+            Vbo.Dispose();
+            Ebo.Dispose();
+            Vao.Dispose();
+            Shader.Dispose();
         }
 
         private static unsafe void OnRender(double obj)
         {
             Gl.Clear((uint)ClearBufferMask.ColorBufferBit);
 
-            Gl.BindVertexArray(Vao);
-            Gl.UseProgram(Shader);
+            Vao.Bind();
+            Shader.Use();
+            Shader.SetUniform("uBlue", (float)Math.Sin(DateTime.Now.Millisecond / 1000f * Math.PI));
 
             Gl.DrawElements(PrimitiveType.Triangles, (uint)Indices.Length, DrawElementsType.UnsignedInt, null);
-        }
-
-        private static void OnUpdate(double obj)
-        {
-            
         }
 
         private static unsafe void OnLoad()
@@ -95,61 +73,14 @@ namespace LearnOpenGL
 
             Gl = GL.GetApi(window);
 
-            Vao = Gl.GenVertexArray();
-            Gl.BindVertexArray(Vao);
+            Ebo = new BufferObject<uint>(Gl, Indices, BufferTargetARB.ElementArrayBuffer);
+            Vbo = new BufferObject<float>(Gl, Vertices, BufferTargetARB.ArrayBuffer);
+            Vao = new VertexArrayObject<float, uint>(Gl, Vbo, Ebo);
 
-            Vbo = Gl.GenBuffer();
-            Gl.BindBuffer(BufferTargetARB.ArrayBuffer, Vbo);
-            fixed (void* v = &Vertices[0])
-            {
-                Gl.BufferData(BufferTargetARB.ArrayBuffer, (nuint)(Vertices.Length * sizeof(uint)), v, BufferUsageARB.StaticDraw);
-            }
+            Vao.VertexAttributePointer(0, 3, VertexAttribPointerType.Float, 7, 0);
+            Vao.VertexAttributePointer(1, 4, VertexAttribPointerType.Float, 7, 3);
 
-            Ebo = Gl.GenBuffer();
-            Gl.BindBuffer(BufferTargetARB.ElementArrayBuffer, Ebo);
-            fixed (void* i = &Indices[0])
-            {
-                Gl.BufferData(BufferTargetARB.ElementArrayBuffer, (nuint)(Indices.Length * sizeof(uint)), i, BufferUsageARB.StaticDraw);
-            }
-
-            uint vertexShader = Gl.CreateShader(ShaderType.VertexShader);
-            Gl.ShaderSource(vertexShader, VertexShaderSource);
-            Gl.CompileShader(vertexShader);
-
-            string infoLog = Gl.GetShaderInfoLog(vertexShader);
-            if (!string.IsNullOrEmpty(infoLog))
-            {
-                System.Console.WriteLine($"Error compiling vertex shader {infoLog}");
-            }
-
-            uint fragmentShader = Gl.CreateShader(ShaderType.FragmentShader);
-            Gl.ShaderSource(fragmentShader, FragmentShaderSource);
-            Gl.CompileShader(fragmentShader);
-
-            infoLog = Gl.GetShaderInfoLog(fragmentShader);
-            if (!string.IsNullOrWhiteSpace(infoLog))
-            {
-                System.Console.WriteLine($"Error compiling fragment shader {infoLog}");
-            }
-
-            Shader = Gl.CreateProgram();
-            Gl.AttachShader(Shader, vertexShader);
-            Gl.AttachShader(Shader, fragmentShader);
-            Gl.LinkProgram(Shader);
-
-            Gl.GetProgram(Shader, GLEnum.LinkStatus, out var status);
-            if (status == 0)
-            {
-                System.Console.WriteLine($"Error linking shader {Gl.GetProgramInfoLog(Shader)}");
-            }
-
-            Gl.DetachShader(Shader, vertexShader);
-            Gl.DetachShader(Shader, fragmentShader);
-            Gl.DeleteShader(vertexShader);
-            Gl.DeleteShader(fragmentShader);
-
-            Gl.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), null);
-            Gl.EnableVertexAttribArray(0);
+            Shader = new Shader(Gl, "shader.vert", "shader.frag");
         }
 
         private static void KeyDown(IKeyboard arg1, Key arg2, int arg3)
