bb423f8c7ee58bac00d71d2f0d0c2ad5297e45ff
Adam Beardow <abeardow13@gmail.com>
2021-04-05 19:14:19 +0100

    Camera

--- LearnOpenGL/Program.cs
+++ LearnOpenGL/Program.cs
@@ -2,6 +2,7 @@
 using Silk.NET.OpenGL;
 using Silk.NET.Windowing;
 using System;
+using System.Linq;
 using System.Numerics;
 
 namespace LearnOpenGL
@@ -10,6 +11,7 @@ namespace LearnOpenGL
     {
         private static IWindow window;
         private static GL Gl;
+        private static IKeyboard primaryKeyboard;
 
         private const int Width = 800;
         private const int Height = 700;
@@ -22,10 +24,14 @@ namespace LearnOpenGL
         private static Shader Shader;
 
         private static Vector3 CameraPosition = new Vector3(0.0f, 0.0f, 3.0f);
-        private static Vector3 CameraTarget = Vector3.Zero;
-        private static Vector3 CameraDirection = Vector3.Normalize(CameraPosition - CameraTarget);
-        private static Vector3 CameraRight = Vector3.Normalize(Vector3.Cross(Vector3.UnitY, CameraDirection));
-        private static Vector3 CameraUp = Vector3.Cross(CameraDirection, CameraRight);
+        private static Vector3 CameraFront = new Vector3(0.0f, 0.0f, -1.0f);
+        private static Vector3 CameraUp = Vector3.UnitY;
+        private static Vector3 CameraDirection = Vector3.Zero;
+        private static float CameraYaw = -90f;
+        private static float CameraPitch = 0f;
+        private static float CameraZoom = 45f;
+
+        private static Vector2 LastMousePosition;
 
         private static readonly float[] Vertices =
         {
@@ -84,14 +90,41 @@ namespace LearnOpenGL
             var options = WindowOptions.Default;
             options.Size = new Silk.NET.Maths.Vector2D<int>(800, 600);
             options.Title = "LearnOpenGL with Silk.NET";
+            options.VSync = false;
 
             window = Window.Create(options);
 
             window.Load += OnLoad;
             window.Render += OnRender;
             window.Closing += OnClose;
+            window.Update += OnUpdate;
 
             window.Run();
+        }
+
+        private static void OnUpdate(double deltaTime)
+        {
+            var moveSpeed = 2.5f * (float)deltaTime;
+
+            if (primaryKeyboard.IsKeyPressed(Key.W))
+            {
+                CameraPosition += moveSpeed * CameraFront;
+            }
+
+            if (primaryKeyboard.IsKeyPressed(Key.S))
+            {
+                CameraPosition -= moveSpeed * CameraFront;
+            }
+
+            if (primaryKeyboard.IsKeyPressed(Key.A))
+            {
+                CameraPosition -= Vector3.Normalize(Vector3.Cross(CameraFront, CameraUp)) * moveSpeed;
+            }
+
+            if (primaryKeyboard.IsKeyPressed(Key.D))
+            {
+                CameraPosition += Vector3.Normalize(Vector3.Cross(CameraFront, CameraUp)) * moveSpeed;
+            }
         }
 
         private static void OnClose()
@@ -115,22 +148,31 @@ namespace LearnOpenGL
             var difference = (float)(window.Time * 100);
 
             var model = Matrix4x4.CreateRotationY(MathHelper.DegreesToRadians(difference)) * Matrix4x4.CreateRotationX(MathHelper.DegreesToRadians(difference));
-            var view = Matrix4x4.CreateLookAt(CameraPosition, CameraTarget, CameraUp);
-            var projection = Matrix4x4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(45.0f), Width / Height, 0.1f, 100.0f);
+            var view = Matrix4x4.CreateLookAt(CameraPosition, CameraPosition + CameraFront, CameraUp);
+            var projection = Matrix4x4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(CameraZoom), Width / Height, 0.1f, 100.0f);
 
             Shader.SetUniform("uModel", model);
             Shader.SetUniform("uView", view);
             Shader.SetUniform("uProjection", projection);
 
             Gl.DrawArrays(PrimitiveType.Triangles, 0, 36);
+
         }
 
         private static unsafe void OnLoad()
         {
             IInputContext input = window.CreateInput();
-            for (int i = 0; i < input.Keyboards.Count; i++)
+            primaryKeyboard = input.Keyboards.FirstOrDefault();
+            if (primaryKeyboard != null)
             {
-                input.Keyboards[i].KeyDown += KeyDown;
+                primaryKeyboard.KeyDown += KeyDown;
+            }
+
+            for (int i = 0; i < input.Mice.Count; i++)
+            {
+                input.Mice[i].Cursor.CursorMode = CursorMode.Raw;
+                input.Mice[i].MouseMove += OnMouseMove;
+                input.Mice[i].Scroll += OnMouseWheel;
             }
 
             Gl = GL.GetApi(window);
@@ -145,6 +187,33 @@ namespace LearnOpenGL
             Shader = new Shader(Gl, "shader.vert", "shader.frag");
 
             Texture = new Texture(Gl, "silk.png"); 
+        }
+
+        private static void OnMouseWheel(IMouse mouse, ScrollWheel scrollWheel)
+        {
+            CameraZoom = Math.Clamp(CameraZoom - scrollWheel.Y, 1.0f, 45f);
+        }
+
+        private static void OnMouseMove(IMouse mouse, Vector2 position)
+        {
+            var lookSensitivity = 0.1f;
+            if (LastMousePosition == default) { LastMousePosition = position; }
+            else
+            {
+                var xOffset = (position.X - LastMousePosition.X) * lookSensitivity;
+                var yOffset = (position.Y - LastMousePosition.Y) * lookSensitivity;
+                LastMousePosition = position;
+
+                CameraYaw += xOffset;
+                CameraPitch -= yOffset;
+
+                CameraPitch = Math.Clamp(CameraPitch, -89.0f, 89.0f);
+
+                CameraDirection.X = MathF.Cos(MathHelper.DegreesToRadians(CameraYaw)) * MathF.Cos(MathHelper.DegreesToRadians(CameraPitch));
+                CameraDirection.Y = MathF.Sin(MathHelper.DegreesToRadians(CameraPitch));
+                CameraDirection.Z = MathF.Sin(MathHelper.DegreesToRadians(CameraYaw)) * MathF.Cos(MathHelper.DegreesToRadians(CameraPitch));
+                CameraFront = Vector3.Normalize(CameraDirection);
+            }
         }
 
         private static void KeyDown(IKeyboard arg1, Key arg2, int arg3)
